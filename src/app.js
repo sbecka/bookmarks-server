7935933d9c4f3a1c2f694e78bb4d1bdd48a8c760
Sabrina Becker <sbecka8@gmail.com>
2020-01-31 14:12:03 -0500

    Configure logging and handle authorization

--- src/app.js
+++ src/app.js
@@ -4,13 +4,25 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-
+const logger = require('./logger');
 const app = express();
 
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res,next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: "Unauthorized request" });
+    }
+
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
---
+++ src/bookmark/bookmark.js
@@ -0,0 +1,7 @@
+const bookmarks = [{
+    id: 1,
+    title: "Cheese",
+    url: "https://en.wikipedia.org/wiki/Cheese",
+    description: "History of cheese",
+    rating: 5
+}]
